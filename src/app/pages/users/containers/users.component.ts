b95420d6b051ffa10cc08d14ef6024b9c8891f44
DavidLinoe <davideduardolino@outlook.com>
2026-04-13 18:56:20 -0300

    feat: configurando primeira rota

---
+++ src/app/pages/users/containers/users.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    templateUrl:'./users.component.html',
+    styles:`
+    
+    .example-spacer {
+  flex: 1 1 auto;
+}
+
+    
+    `
+})
+export class UsersComponent{
+
+
+}
