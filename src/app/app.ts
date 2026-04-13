4f45c31aa0b5c20430eab6673d1fec2aefa4a8b1
DavidLinoe <davideduardolino@outlook.com>
2026-04-13 19:57:28 -0300

    refactor: removi o filtro do app o correto seria um navigation que gerencia esse filtro passando o valor para o componente usuaris

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatIconModule,MatFormFieldModule,MatInputModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/pages/users/containers/users.component.ts
+++ src/app/pages/users/containers/users.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { UsersListComponent } from '../components/usersList/usersList.component';
 import { UsersList } from '../models/users.models';
 import { UsersApi } from '../api/users.api';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
-  imports: [UsersListComponent],
+  imports: [UsersListComponent, MatToolbar, MatIconModule, MatFormFieldModule, MatInputModule],
   providers: [UsersApi],
 })
 export class UsersComponent {
