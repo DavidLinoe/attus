9e744a5af7ce10bb82fae406b30ad2ebfb5d0ffb
DavidLinoe <davideduardolino@outlook.com>
2026-04-15 18:51:10 -0300

    feat: adicionando buttom mat module

--- src/app/pages/users/components/usersList/usersList.component.ts
+++ src/app/pages/users/components/usersList/usersList.component.ts
@@ -1,14 +1,13 @@
 import { Component, input, output } from '@angular/core';
-import { MatListItem, MatListItemIcon, MatNavList } from '@angular/material/list';
 import { UsersList } from '../../models/users.models';
 import { MatIcon } from '@angular/material/icon';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'attus-users-list',
   templateUrl: './usersList.component.html',
   styleUrls: ['./usersList.component.css'],
-  imports: [MatNavList, MatIcon, MatListItem, MatListItemIcon, MatGridListModule],
+  imports: [MatIcon, MatButtonModule],
 })
 export class UsersListComponent {
   users = input.required<UsersList[]>();
