3e7a3356af41797a7f14667c575535a01827d8ff
DavidLinoe <davideduardolino@outlook.com>
2026-04-16 17:46:39 -0300

    feat: adicionando configuração do ngrx para gerenciamento de estado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { routes } from './app.routes';
+import { todoReducer } from './pages/todo/store/todo.reducer';
+import { TodoEffects } from './pages/todo/store/todo.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore({ todos: todoReducer }),
+    provideEffects([TodoEffects]),
+  ],
 };
