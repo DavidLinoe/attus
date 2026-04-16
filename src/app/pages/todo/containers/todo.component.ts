db5660cc89e71329c83583246094866c305403da
DavidLinoe <davideduardolino@outlook.com>
2026-04-16 18:13:44 -0300

    feat: criando componente base do todo list

--- src/app/pages/todo/containers/todo.component.ts
+++ src/app/pages/todo/containers/todo.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TodoFacade } from '../facades/todo.facade';
+import { Todo } from '../models/todo.models';
 
 @Component({
   templateUrl: './todo.component.html',
+  imports: [AsyncPipe, MatCheckboxModule, MatProgressSpinnerModule],
+  providers: [TodoFacade],
 })
-export class TodoComponent {}
+export class TodoComponent {
+  constructor(public todoFacade: TodoFacade) {
+    this.todoFacade.getAll();
+    this.todoFacade.todos$.subscribe((list: Todo[]) => {
+      console.log(list);
+    });
+  }
+
+  toggle(id: number) {
+    this.todoFacade.toggle(id);
+  }
+}
